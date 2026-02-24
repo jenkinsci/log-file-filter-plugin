3b164a91f3744e41db7eefd6d9c99cb2832a01fb
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-02-24 09:10:32 +0100

    Migrate tests to JUnit Jupiter
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup
    * Ban JUnit4 imports

--- src/test/java/com/tsystems/sbs/DefaultRegexpPairsAWSTest.java
+++ src/test/java/com/tsystems/sbs/DefaultRegexpPairsAWSTest.java
@@ -1,38 +1,34 @@
 package com.tsystems.sbs;
 
-import hudson.console.LineTransformationOutputStream;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.codehaus.groovy.runtime.ResourceGroovyMethods.filterLine;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class DefaultRegexpPairsAWSTest {
 
-public class DefaultRegexpPairsAWSTest {
     private List<RegexpPair> getDefaultRegexpPairs() {
         return DefaultRegexpPairs.getDefaultRegexesAWS();
     }
+
     @Test
-    public void testDefaultPairsList() {
+    void testDefaultPairsList() {
         List<RegexpPair> defaultRegexpPairs = getDefaultRegexpPairs();
         assertThat(defaultRegexpPairs.size(), greaterThan(0));
-
     }
 
     @Test
-    public void testDefaultPairs() {
+    void testDefaultPairs() {
         List<RegexpPair> defaultRegexpPairs = getDefaultRegexpPairs();
 
         // Define the input string
         String input = "AWS_ACCESS_KEY_ID=R2RHTXG7QKSRWMOHEIAMH4A AWS_SECRET_ACCESS_KEY=/lD8T9bXuZUW/F/8MutOB1vDXK2uG/gNHUe/d8bG AWS_SESSION_TOKEN=Z1XKqTnKIHd7eLJhBZb9QWVcG0Rj3f8z1uYgO4Xm6vNiD5F7cM9pAa/2SsPqRrTtEoUyHwC+DxGJlWbVfNkOYK6hI3eX1L0j2+//////////";
         String expected = "AWS_ACCESS_KEY_ID=******** AWS_SECRET_ACCESS_KEY=******** AWS_SESSION_TOKEN=********";
-
 
         StringBuilder replacedInput = new StringBuilder(input);
 
@@ -65,4 +61,3 @@ public class DefaultRegexpPairsAWSTest {
         assertEquals(expected, replacedInputString);
     }
 }
-
--- src/test/java/com/tsystems/sbs/DefaultRegexpPairsTest.java
+++ src/test/java/com/tsystems/sbs/DefaultRegexpPairsTest.java
@@ -1,18 +1,18 @@
 package com.tsystems.sbs;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class DefaultRegexpPairsTest {
+class DefaultRegexpPairsTest {
+
     /** Sorry, this is really just a dummy testcase. */
     @Test
-    public void testDefaultPairs() {
+    void testDefaultPairs() {
         List<RegexpPair> defaultRegexpPairs = DefaultRegexpPairs.getDefaultRegexes();
         assertThat(defaultRegexpPairs.size(), is(2));
-
     }
 }
